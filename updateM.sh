e9bba2c4f244a61f251380394069110f3a9b8dab
Marshamallow37 <Liam@cervenka.space>
2022-10-03 11:22:32 +0800

    Created and sh update file for my mac directory

---
+++ updateM.sh
@@ -0,0 +1,8 @@
+#!usr/bin/bash
+YELLOW='\033[0;93m'
+NC='\033[0m' 
+echo "${YELLOW}Update begins! ${NC}"
+cd /Users/liamcervenka/Music/VehicularManslaughter
+echo "${YELLOW}Navigated! ${NC}"
+spotdl https://open.spotify.com/playlist/1aspn6wkVaDVrAR5KZ3g7t?si=3f42f33b4a904e66
+echo "${YELLOW}Update Complete! ${NC}"
